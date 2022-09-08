7006bd31975cea05b275ff29acfb7ba77d1a54da
Brady Buchholz <bradybuchholz@gmail.com>
2022-09-08 16:14:45 -0400

    added avaliable meals array and meal summary component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpeg';
+import CartButton from './HeaderCartButton';
 function Header(props) {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>Bubba's Meals</h1>
-        <button>Cart</button>
+        <CartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
