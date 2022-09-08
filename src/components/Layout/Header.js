2b62866c0859bcad6b2447a752ab5bf8e0fcf357
Brady Buchholz <bradybuchholz@gmail.com>
2022-09-07 23:16:04 -0400

    first image and header styling

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpeg';
 function Header(props) {
   return (
     <React.Fragment>
-      <header>
+      <header className={classes.header}>
         <h1>Bubba's Meals</h1>
+        <button>Cart</button>
       </header>
-      <div></div>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
     </React.Fragment>
   );
 }
