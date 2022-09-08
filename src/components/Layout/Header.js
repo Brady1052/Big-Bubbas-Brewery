611ecd54d17f48bacd889e416de38c30302c00e8
Brady Buchholz <bradybuchholz@gmail.com>
2022-09-07 23:01:24 -0400

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Header.module.css';
+function Header(props) {
+  return (
+    <React.Fragment>
+      <header>
+        <h1>Bubba's Meals</h1>
+      </header>
+      <div></div>
+    </React.Fragment>
+  );
+}
+
+export default Header;
